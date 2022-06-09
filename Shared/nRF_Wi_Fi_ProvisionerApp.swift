d1be818c59590f60e99b48b35933c4d83fad37ca
Nick Kibysh <nick.kibysh@gmail.com>
2022-06-09 12:57:45 +0200

    Created project

---
+++ Shared/nRF_Wi_Fi_ProvisionerApp.swift
@@ -0,0 +1,17 @@
+//
+//  nRF_Wi_Fi_ProvisionerApp.swift
+//  Shared
+//
+//  Created by Nick Kibysh on 09/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct nRF_Wi_Fi_ProvisionerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
