561394ddfaacad44ad0ec79715ebbb7d0ad1cd5c
Nick Kibysh <nick.kibysh@gmail.com>
2022-12-12 10:10:48 +0100

    docs(Provisioner 2.0): Added remarks and preconditions to DeviceManager

--- Sources/NordicWiFiProvisioner/Provisioner/DeviceManager.swift
+++ Sources/NordicWiFiProvisioner/Provisioner/DeviceManager.swift
@@ -15,20 +15,28 @@ open class DeviceManager {
     /// The device identifier. It is the same as the device's UUID.
     public let deviceId: String
     
-    
     /// Initialize a new instance of the `DeviceManager`.
+    ///
+    /// - Remark: See also: ``init(scanResult:)``
+    /// - Precondition: `deviceId` should be a valid UUID String, otherwise ``connect()`` method will fail.
     /// - Parameter deviceId: ID of the device. It's equal to the `CBPeripheral`'s UUID String.
     public init(deviceId: String) {
         self.deviceId = deviceId
     }
     
     /// Initialize a new instance of the `DeviceManager`.
+    ///
+    /// - Remark: See also: ``init(deviceId:)``
     /// - Parameter scanResult: ``ScanResult`` discovered by ``Scanner``
     convenience public init(scanResult: ScanResult) {
         self.init(deviceId: scanResult.id)
     }
     
     /// Bluetooth Connection State.
+    ///
+    /// This property represents connection to the nRF-7 device (peripheral). Make sure it's ``ConnectionState-swift.enum/connected`` before try to communicate with the device.
+    ///
+    /// - Remark: See also: ``connect()`` and ``ConnectionDelegate/deviceManager(_:changedConnectionState:)``
     open var connectionState: ConnectionState {
         internalProvisioner.connectionState
     }
@@ -75,9 +83,12 @@ open class DeviceManager {
     
     /// Connect to the device.
     ///
-    /// Make sure that iOS or MacOS device supports bluetooth and it's turned on and ready to use.
+    /// Result of the connection will be delivered to ``ConnectionDelegate/deviceManagerConnectedDevice(_:)`` or ``ConnectionDelegate/deviceManagerDidFailToConnect(_:error:)``.
     ///
-    /// Result of the connection will be delivered to ``ConnectionDelegate/deviceManager(_:changedConnectionState:)`` or ``ConnectionDelegate/deviceManagerDidFailToConnect(_:error:)``. New ``connectionState-swift.property`` will be delivered to ``ConnectionDelegate/deviceManager(_:changedConnectionState:)``
+    /// ``connectionState-swift.property`` will be updated. Also the new state will be delivered to ``ConnectionDelegate/deviceManager(_:changedConnectionState:)``
+    ///
+    /// - Remark: See also: ``connectionState-swift.property`` and ``ConnectionDelegate``
+    /// - Precondition: Make sure that iOS or MacOS device supports bluetooth and it's turned on and ready to use.
     open func connect() {
         internalProvisioner.connect()
     }
@@ -85,8 +96,8 @@ open class DeviceManager {
     /// Read the device version.
     ///
     /// The result will be delivered to ``InfoDelegate/versionReceived(_:)``
-    ///
-    /// - Throws: If the version was request but device is not connected, this method throws ``DeviceNotConnectedError``. You should call ``connect()`` before calling this method.
+    /// - Precondition: Make sure that ``connectionState-swift.property`` is ``ConnectionState-swift.enum/connected`` before calling this method.
+    /// - Throws: If the version was request but device is not connected, this method throws ``DeviceNotConnectedError``.
     open func readVersion() throws {
         try internalProvisioner.readVersion()
     }
@@ -95,7 +106,8 @@ open class DeviceManager {
     ///
     /// The result of this method is delivered to ``InfoDelegate/deviceStatusReceived(_:)``
     ///
-    /// - Throws: If the status was request but device is not connected, this method throws ``DeviceNotConnectedError``. You should call ``connect()`` before calling this method.
+    /// - Precondition: Make sure that ``connectionState-swift.property`` is ``ConnectionState-swift.enum/connected`` before calling this method.
+    /// - Throws: If the status was request but device is not connected, this method throws ``DeviceNotConnectedError``.
     open func readDeviceStatus() throws {
         try internalProvisioner.readDeviceStatus()
     }
@@ -104,40 +116,52 @@ open class DeviceManager {
     ///
     /// The result of this method is delivered to ``WiFiScanerDelegate/deviceManager(_:discoveredAccessPoint:rssi:)``. Also ``WiFiScanerDelegate/deviceManagerDidStartScan(_:error:)`` will be called.
     ///
+    /// - Remark: See also: ``startScan(band:passive:period:groupChannels:)``
+    /// - Precondition: Make sure that ``connectionState-swift.property`` is ``ConnectionState-swift.enum/connected`` before calling this method.
     /// - Parameter scanParams: Scan parameters
-    /// - Throws: If the scan was request but device is not connected, this method throws ``DeviceNotConnectedError``. You should call ``connect()`` before calling this method.
+    /// - Throws: If the scan was request but device is not connected, this method throws ``DeviceNotConnectedError``.
     open func startScan(scanParams: ScanParams) throws {
         try internalProvisioner.startScan(scanParams: scanParams)
-    }
-    
-    /// Stop scan for Wi-Fi networks.
-    ///
-    /// ``WiFiScanerDelegate/deviceManagerDidStopScan(_:error:)`` wil be called.
-    ///
-    /// - Throws: If the `stopScan` was called but device is not connected, this method throws ``DeviceNotConnectedError``. You should call ``connect()`` before calling this method.
-    open func stopScan() throws {
-        try internalProvisioner.stopScan()
     }
 
     /// Start scan for Wi-Fi networks
     ///
     /// The result of this method is delivered to ``WiFiScanerDelegate/deviceManager(_:discoveredAccessPoint:rssi:)``. Also ``WiFiScanerDelegate/deviceManagerDidStartScan(_:error:)`` will be called.
     ///
+    /// - Precondition: Make sure that ``connectionState-swift.property`` is ``ConnectionState-swift.enum/connected`` before calling this method.
+    ///
     /// - Parameters:
     ///   - band: Band to scan
     ///   - passive: TODO: What is this?
     ///   - period: Period of scan in milliseconds
     ///   - groupChannels: TODO: What is this?
-    /// - Throws: If the scan was request but device is not connected, this method throws ``DeviceNotConnectedError``. You should call ``connect()`` before calling this method.
+    ///
+    /// - Throws: If the scan was request but device is not connected, this method throws ``DeviceNotConnectedError``.
+    ///
+    /// - Remark: See also: ``startScan(scanParams:)``
     open func startScan(band: ScanParams.Band = .any, passive: Bool = true, period: UInt, groupChannels: UInt) throws {
         let scanParams = ScanParams(band: band, passive: passive, periodMs: period, groupChannels: groupChannels)
         try self.startScan(scanParams: scanParams)
+    }
+    
+    /// Stop scan for Wi-Fi networks.
+    ///
+    /// ``WiFiScanerDelegate/deviceManagerDidStopScan(_:error:)`` wil be called.
+    ///
+    /// - Precondition: Make sure that ``connectionState-swift.property`` is ``ConnectionState-swift.enum/connected`` before calling this method.
+    ///
+    /// - Throws: If the `stopScan` was called but device is not connected, this method throws ``DeviceNotConnectedError``.
+    open func stopScan() throws {
+        try internalProvisioner.stopScan()
     }
     
     /// Start provisioning.
     ///
     /// Set Wi-Fi configuration to the device. The result of this method is delivered to ``ProvisionDelegate/deviceManagerDidSetConfig(_:error:)``. If the new configuration is set successfully, the device will try to connect to provided Wi-Fi network. Wi-Fi connection status will be sent to ``ProvisionDelegate/deviceManager(_:didChangeState:)``.
     ///
+    /// - Precondition: Make sure that ``connectionState-swift.property`` is ``ConnectionState-swift.enum/connected`` before calling this method.
+    ///
+    /// - Remark: See also: ``setConfig(wifi:passphrase:volatileMemory:)``
     /// - Parameters:
     ///   - config: Wi-Fi configuration
     /// - Throws: If the provisioning was request but device is not connected, this method throws ``DeviceNotConnectedError``. You should call ``connect()`` before provisioning.
@@ -149,6 +173,9 @@ open class DeviceManager {
     ///
     /// Set Wi-Fi configuration to the device. The result of this method is delivered to ``ProvisionDelegate/deviceManagerDidSetConfig(_:error:)``. If the new configuration is set successfully, the device will try to connect to provided Wi-Fi network. Wi-Fi connection status will be sent to ``ProvisionDelegate/deviceManager(_:didChangeState:)``.
     ///
+    /// - Precondition: Make sure that ``connectionState-swift.property`` is ``ConnectionState-swift.enum/connected`` before calling this method.
+    ///
+    /// - Remark: See also: ``setConfig(_:)``
     /// - Parameters:
     ///   - wifi: Wi-Fi network information
     ///   - passphrase: Wi-Fi network passphrase. If the network is open, this parameter should be `nil`.
@@ -163,7 +190,9 @@ open class DeviceManager {
     ///
     /// The result of this method is delivered to ``ProvisionDelegate/deviceManagerDidForgetConfig(_:error:)``.
     ///
-    /// - Throws: If the unprovisioning was requested but device is not connected, this method throws ``DeviceNotConnectedError``. You should call ``connect()`` before calling this method.
+    /// - Precondition: Make sure that ``connectionState-swift.property`` is ``ConnectionState-swift.enum/connected`` before calling this method.
+    ///
+    /// - Throws: If the unprovisioning was requested but device is not connected, this method throws ``DeviceNotConnectedError``.
     open func forgetConfig() throws {
         try internalProvisioner.forgetConfig()
     }
