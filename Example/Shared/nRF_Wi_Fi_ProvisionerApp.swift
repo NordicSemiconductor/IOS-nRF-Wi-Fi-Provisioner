5ef6455bd85d9450c1bc7da5fbaf946bcd00a3e3
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2024-02-12 14:42:09 +0100

    Select Mode at App Start
    Looks very bad.

--- Example/Shared/nRF_Wi_Fi_ProvisionerApp.swift
+++ Example/Shared/nRF_Wi_Fi_ProvisionerApp.swift
@@ -11,20 +11,28 @@ import NordicWiFiProvisioner
 @main
 struct nRF_Wi_Fi_ProvisionerApp: App {
     
+    @State private var mode: SelectorView.Mode?
+    
     init() {
         MockManager.emulateDevices()
     }
     
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                ScannerView(viewModel: ScannerViewModel())
-                    .environmentObject(DeviceViewModelFactory())
-                    .onFirstAppear {
-                        
-                    }
+            switch mode {
+            case .none:
+                SelectorView(selected: $mode)
+            case .provisionOverBle:
+                NavigationView {
+                    ScannerView(viewModel: ScannerViewModel())
+                        .environmentObject(DeviceViewModelFactory())
+                        .onFirstAppear {
+                            
+                        }
+                }
+            case .provisionOverWifi:
+                Text("To-Do")
             }
         }
-                
     }
 }
