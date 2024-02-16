838a6be1c4ad156296ee136807fb17f7f6ea190e
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2024-02-16 12:42:01 +0000

    Refactor: NSURLSessionPinningDelegate is now on its own file
    There's a lot of stuff in ProvisionManager. I'm not going to fix and break out all of the code from it into separate files. But this one seemed important enough.

--- Sources/NordicWiFiProvisioner-SoftAP/ProvisionManager.swift
+++ Sources/NordicWiFiProvisioner-SoftAP/ProvisionManager.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  ProvisionManager.swift
+//  NordicWiFiProvisioner-SoftAP
 //
 //  Created by Nick Kibysh on 12/02/2024.
 //
@@ -132,82 +132,6 @@ open class ProvisionManager {
     
     func provision(ssid: String, password: String) async throws {
         
-    }
-}
-
-class NSURLSessionPinningDelegate: NSObject, URLSessionDelegate {
-    static let shared = NSURLSessionPinningDelegate()
-    
-    override init() {
-        super.init()
-    }
-    
-    func urlSession(_ session: URLSession,
-                      didReceive challenge: URLAuthenticationChallenge,
-                      completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Swift.Void) {
-
-        if (challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust) {
-            if let serverTrust = challenge.protectionSpace.serverTrust {
-                Task {
-                    let result = await shouldAllowHTTPSConnection(trust: serverTrust)
-                    if result == true {
-                        completionHandler(URLSession.AuthChallengeDisposition.useCredential, URLCredential(trust: serverTrust))
-                    } else {
-                        completionHandler(URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge, nil)
-                    }
-                }
-            }
-        }
-    }
-
-    func shouldAllowHTTPSConnection(chain: [SecCertificate]) async throws -> Bool {
-        
-        let b = Bundle(for: NSURLSessionPinningDelegate.self)
-        guard let res = b.url(forResource: "Res", withExtension: "bundle") else {
-            fatalError()
-        }
-        guard let resBundle = Bundle(url: res) else {
-            fatalError()
-        }
-        let anchor = resBundle.certificateNamed("certificate")!
-        
-        let policy = SecPolicyCreateBasicX509()
-        let trust = try secCall { SecTrustCreateWithCertificates(chain as NSArray, policy, $0) }
-        let err = SecTrustSetAnchorCertificates(trust, [anchor] as NSArray)
-        guard err == errSecSuccess else {
-            throw NSError(domain: NSOSStatusErrorDomain, code: Int(err), userInfo: nil)
-        }
-
-        var secresult: CFError? = nil
-        let status = SecTrustEvaluateWithError(trust, &secresult)
-        return status
-    }
-
-    func shouldAllowHTTPSConnection(trust: SecTrust) async -> Bool {
-        guard let chain = SecTrustCopyCertificateChain(trust) as? [SecCertificate] else { return false }
-        do {
-            return try await shouldAllowHTTPSConnection(chain: chain)
-        } catch {
-            return false
-        }
-    }
-
-    func secCall<Result>(_ body: (_ resultPtr: UnsafeMutablePointer<Result?>) -> OSStatus  ) throws -> Result {
-        var result: Result? = nil
-        let err = body(&result)
-
-        guard err == errSecSuccess else {
-            throw NSError(domain: NSOSStatusErrorDomain, code: Int(err), userInfo: nil)
-        }
-        return result!
-    }
-    
-    func urlSession(_ session: URLSession, didBecomeInvalidWithError error: Error?) {
-        print(#function)
-    }
-    
-    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
-        print(#function)
     }
 }
 
