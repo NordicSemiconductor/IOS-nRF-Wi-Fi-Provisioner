84d9d3cc93d41e5cb39661368459c11e7c72977f
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2024-02-16 12:47:38 +0000

    Changes to make ProvisionOverWiFi Look Better
    Baby steps.

--- Example/Shared/WiFi/ProvisionOverWiFiView.swift
+++ Example/Shared/WiFi/ProvisionOverWiFiView.swift
@@ -23,68 +23,80 @@ struct ProvisionOverWiFiView: View {
     @State private var ssids: [String] = []
     
     var body: some View {
-        switch status {
-        case .notConnected:
-            Button("Attempt to Connect") {
-                Task {
-                    do {
-                        try await manager.connect()
-                        status = .connected
-                    } catch let e as NSError {
-                        if e.code == 13 {
+        ZStack {
+            switch status {
+            case .notConnected:
+                Button("Attempt to Connect") {
+                    Task {
+                        do {
+                            try await manager.connect()
                             status = .connected
+                        } catch let e as NSError {
+                            if e.code == 13 {
+                                status = .connected
+                            }
                         }
                     }
                 }
-            }
-        case .connected:
-            List {
-                HStack {
-                    Button {
-                        Task {
-                            try await manager.setLED(ledNumber:1, enabled: !led1Enabled)
-                            led1Enabled.toggle()
+            case .connected:
+                List {
+                    Section("LED Testing") {
+                        HStack {
+                            Button {
+                                Task {
+                                    try await manager.setLED(ledNumber:1, enabled: !led1Enabled)
+                                    led1Enabled.toggle()
+                                }
+                            } label: {
+                                Image(systemName: led1Enabled ? "lamp.table.fill" : "lamp.table")
+                            }
+                            .frame(maxWidth: .infinity)
+                            
+                            Divider()
+                            
+                            Button {
+                                Task {
+                                    try await manager.setLED(ledNumber:2, enabled: !led2Enabled)
+                                    led2Enabled.toggle()
+                                }
+                            } label: {
+                                Image(systemName: led2Enabled ? "lamp.table.fill" : "lamp.table")
+                            }
+                            .frame(maxWidth: .infinity)
                         }
-                    } label: {
-                        Image(systemName: led1Enabled ? "lamp.table.fill" : "lamp.table")
                     }
                     
-                    Button {
-                        Task {
-                            try await manager.setLED(ledNumber:2, enabled: !led2Enabled)
-                            led2Enabled.toggle()
+                    Section("Wi-Fi Scanning") {
+                        ForEach(ssids, id: \.self) { ssid in
+                            NavigationLink {
+                                Text(ssid)
+                                    .navigationTitle(Text(ssid))
+                            } label: {
+                                Text(ssid)
+                            }
                         }
-                    } label: {
-                        Image(systemName: led2Enabled ? "lamp.table.fill" : "lamp.table")
+                        Button("Read SSID") {
+                            Task {
+                                self .ssids = try await manager.getSSIDs()
+                            }
+                        }
                     }
                 }
-                ForEach(ssids, id: \.self) { ssid in
-                    NavigationLink {
-                        Text(ssid)
-                            .navigationTitle(Text(ssid))
-                    } label: {
-                        Text(ssid)
-                    }
-                }
-                Button("Read SSID") {
+                .task {
                     Task {
-                        self .ssids = try await manager.getSSIDs()
+                        do {
+                            led1Enabled = try await manager.ledStatus(ledNumber: 1)
+                            led2Enabled = try await manager.ledStatus(ledNumber: 2)
+                        } catch let e {
+                            print(e.localizedDescription)
+                        }
                     }
                 }
+            case .error(let error):
+                Text("Error: \(error.localizedDescription)")
             }
-            .task {
-                Task {
-                    do {
-                        led1Enabled = try await manager.ledStatus(ledNumber: 1)
-                        led2Enabled = try await manager.ledStatus(ledNumber: 2)
-                    } catch let e {
-                        print(e.localizedDescription)
-                    }
-                }
-            }
-        case .error(let error):
-            Text("Error: \(error.localizedDescription)")
         }
+        .navigationTitle("Provision over Wi-Fi")
     }
 }
 
