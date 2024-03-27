0953ba127e272952a2ccba21295482695dc683c3
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2024-03-27 16:40:22 +0000

    Deleted more files & code we're not using
    Just cleanup.

--- Example/Shared/Utils/Extensions/Int+Ext.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Int+Ext.swift
-//  nRF-Wi-Fi-Provisioner
-//
-//  Created by Nick Kibysh on 16/11/2022.
-//
-
-import Foundation
--- Example/Shared/Utils/Extensions/Label+Ext.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Label+Ext.swift
-//  nRF-Wi-Fi-Provisioner
-//
-//  Created by Nick Kibysh on 09/08/2022.
-//
-
-import Foundation
-
