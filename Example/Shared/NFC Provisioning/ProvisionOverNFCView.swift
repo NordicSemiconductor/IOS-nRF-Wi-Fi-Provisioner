e68f7e484d4de6dcda81e6d231606b6863cb6d63
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2024-05-08 13:04:33 +0100

    NFC Provisioning worked !!???
    Source of copy / paste: https://stackoverflow.com/questions/57832849/creating-an-ndef-wifi-record-using-application-vnd-wfa-wsc-in-swift

--- Example/Shared/NFC Provisioning/ProvisionOverNFCView.swift
+++ Example/Shared/NFC Provisioning/ProvisionOverNFCView.swift
@@ -54,7 +54,7 @@ struct ProvisionOverNFCView: View {
                 writeTag()
             }
             .frame(maxWidth: .infinity)
-            .disabled(ssid.isEmpty)
+//            .disabled(ssid.isEmpty)
         }
         .navigationTitle("Provision Over NFC")
     }
@@ -62,14 +62,16 @@ struct ProvisionOverNFCView: View {
     // MARK: Private
     
     private func writeTag() {
-        let delegate = NFCSessionDelegate()
-        let session = NFCNDEFReaderSession(delegate: delegate, queue: nil, invalidateAfterFirstRead: false)
         session.alertMessage = "Hold your iPhone near an NDEF tag to write the message."
         session.begin()
     }
 }
 
 final class NFCSessionDelegate: NSObject, NFCNDEFReaderSessionDelegate {
+    
+    func readerSessionDidBecomeActive(_ session: NFCNDEFReaderSession) {
+        print(#function)
+    }
     
     func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: any Error) {
         print(#function)
@@ -115,15 +117,50 @@ final class NFCSessionDelegate: NSObject, NFCNDEFReaderSessionDelegate {
                     session.alertMessage = "Tag is read only."
                     session.invalidate()
                 case .readWrite:
-//                    tag.writeNDEF(self.message, completionHandler: { (error: Error?) in
-//                        if nil != error {
-//                            session.alertMessage = "Write NDEF message fail: \(error!)"
-//                        } else {
-//                            session.alertMessage = "Write NDEF message successful."
-//                        }
-//                        session.invalidate()
-//                    })
-                    break
+                    let type = "application/vnd.wfa.wsc".data(using: .utf8)!
+
+                    let authenticationTypesString = ["Open", "WPA-Personal", "Shared", "WPA-Enterprise", "WPA2-Enterprise", "WPA2-Personal"]
+                    let encryptionTypesString = ["None", "WEP", "TKIP", "AES", "AES/TKIP (mixed)"]
+
+                    let authenticationTypes: [[UInt8]] = [[0x00, 0x01], [0x00, 0x02], [0x00, 0x04], [0x00, 0x08], [0x00, 0x10], [0x00, 0x20]]
+                    let encryptionTypes: [[UInt8]] = [[0x00, 0x01], [0x00, 0x02], [0x00, 0x04], [0x00, 0x08], [0x00, 0x0c]]
+
+                    let ssidString = ""
+                    let networkKeyString = ""
+
+                    let ssidBytes: [UInt8] = Array(ssidString.utf8)
+                    let networkKeyBytes: [UInt8] = Array(networkKeyString.utf8)
+
+                    let ssidLength = UInt8(ssidBytes.count)
+                    let networkKeyLength = UInt8(networkKeyBytes.count)
+
+                    let authenticationTypeBytes = authenticationTypes[5]
+                    let encryptionTypeBytes = encryptionTypes[3]
+
+                    let networkIndex: [UInt8] = [0x10, 0x26, 0x00, 0x01, 0x01]
+                    let ssid: [UInt8] = [0x10, 0x45, 0x00, ssidLength] + ssidBytes
+                    let authenticationType: [UInt8] = [0x10, 0x03, 0x00, 0x02] + authenticationTypeBytes
+                    let encryptionType: [UInt8] = [0x10, 0x0F, 0x00, 0x02] + encryptionTypeBytes
+                    let networkKey: [UInt8] = [0x10, 0x27, 0x00, networkKeyLength] + networkKeyBytes
+                    let macAddress: [UInt8] = [0x10, 0x20, 0x00, 0x06, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+                    let credential = networkIndex + ssid + authenticationType + encryptionType + networkKey + macAddress
+                    let credentialLength = UInt8(credential.count)
+
+                    let bytes = [0x10, 0x0E, 0x00, credentialLength] + credential
+                    let payload = Data(bytes: bytes, count: bytes.count)
+
+                    let ndefPayload = NFCNDEFPayload(format: .media, type: type, identifier: Data(), payload: payload)
+
+                    let message = NFCNDEFMessage(records: [ndefPayload])
+                    tag.writeNDEF(message, completionHandler: { (error: Error?) in
+                        if nil != error {
+                            session.alertMessage = "Write NDEF message fail: \(error!)"
+                        } else {
+                            session.alertMessage = "Write NDEF message successful."
+                        }
+                        session.invalidate()
+                    })
                 @unknown default:
                     session.alertMessage = "Unknown NDEF tag status."
                     session.invalidate()
