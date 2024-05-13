c5e04b99bec82a1340d705b46f828379c4e9bd8b
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2024-05-13 10:57:30 +0100

    Ongoing changes to IntroView
    We need to bring this up to date to the current state / features of the application.

--- Example/Shared/Intro/IntroView.swift
+++ Example/Shared/Intro/IntroView.swift
@@ -1,13 +1,18 @@
 //
-//  ContentView.swift
-//  Shared
+//  IntroView.swift
+//  nRF-Wi-Fi-Provisioner (iOS)
 //
 //  Created by Nick Kibysh on 09/06/2022.
 //
 
 import SwiftUI
 
+// MARK: - IntroView
+
 struct IntroView: View {
+    
+    // MARK: Properties
+    
     @State private var animationAmount = 1.0
     @State private var selection: Int? = nil
     
@@ -16,69 +21,62 @@ struct IntroView: View {
     @Binding var show: Bool
     @Binding var dontShowAgain: Bool
     
+    // MARK: View
+    
     var body: some View {
         NavigationView {
-            ScrollView {
-                
-                Image(viewModel.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .padding()
-                
-                VStack(alignment: .leading) {
-                    Text("The application allows provisioning of a new nRF700x device to Wi-Fi networks over Bluetooth LE.")
-                        .padding()
-                    VStack(alignment: .leading) {
-                        Text("**Key features:**")
-                            .font(.title2)
-                        Text("• Provision of a new device to the desired network.")
-                        Text("• Get the connection status of the provisioned device.")
-                        Text("• Re-provision already provisioned devices to an alternate network.")
-                    }
-                    .padding()
-                    VStack(alignment: .leading) {
-                        Text("**Requirements:**")
-                            .font(.title2)
-                        Text("• [nRF700x DK](https://nordicsemi.no)")
-                        Text("• Provisioning sample app flashed")
-                    }
-                    .padding()
+            List {
+                Section {
+                    Image(viewModel.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                        .listRowSeparator(.hidden)
                     
-                    Text("Make sure the device is powered ON and in range of the phone.")
-                        .padding()
+                    Text("Version: \(viewModel.version)")
+                        .font(.caption)
+                        .frame(maxWidth: .infinity)
                 }
                 
-                Text("Version: \(viewModel.version)")
-                    .multilineTextAlignment(.trailing)
-                    .font(.caption)
-                    .padding()
+                Section {
+                    VStack(alignment: .leading) {
+                        Text("Features:")
+                            .bold()
+                            .font(.title2)
+                        
+                        Text("• Provision of a device over BLE (Bluetooth Low Energy), Wi-Fi (Access Point) or NFC (Near-Field Communication)")
+                        Text("• (Optionally) verify correct provisioning of the device.")
+                        Text("• Re-provision already provisioned devices to an alternate network.")
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("Requirements:")
+                            .bold()
+                            .font(.title2)
+                        
+                        Text("• [nRF700x DK](https://nordicsemi.no)")
+                        Text("• Corresponding provisioning application running on device (each provisioning method requires a specific firmware)")
+                    }
+                    
+                    Text("Make sure the device is powered ON, in range and correct firmware is flashed.")
+                }
+                .listRowSeparator(.hidden)
                 
                 HStack {
                     Toggle("Don't show again", isOn: $dontShowAgain)
                 }
-                .padding()
                 
                 Button("START_PROVISIONING_BTN") {
                     show = false
                 }
-                .padding()
+                .buttonStyle(.borderedProminent)
+                .frame(maxWidth: .infinity)
+                .padding(.bottom)
+                .listRowSeparator(.hidden)
                 .accessibilityIdentifier("start_provisioning_btn")
             }
             .navigationTitle("Welcome")
             .onAppear { try? viewModel.readInfo() }
-            
-            
         }
     }
 }
-
-#if DEBUG
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            IntroView(show: .constant(true), dontShowAgain: .constant(false))
-        }
-    }
-}
-#endif
