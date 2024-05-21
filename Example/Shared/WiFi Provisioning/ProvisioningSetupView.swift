fd8ec4ff9897ce9f77425a02588db9f18deabe97
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2024-05-21 12:03:23 +0100

    Fixed ProvisioningSetupView background color in Light Mode
    That's something we missed yesterday. But we caught it today.

--- Example/Shared/WiFi Provisioning/ProvisioningSetupView.swift
+++ Example/Shared/WiFi Provisioning/ProvisioningSetupView.swift
@@ -22,52 +22,55 @@ struct ProvisioningSetupView: View {
     // MARK: View
     
     var body: some View {
-        List {
-            Section("Device Selection") {
-                Toggle(isOn: $switchToAccessPoint) {
-                    Label("Connect Automatically", systemImage: "cpu")
-                }
-                .tint(.accentColor)
-                
-                if switchToAccessPoint {
-                    HStack {
-                        Label("Device SSID", systemImage: "wifi.circle")
+        VStack {
+            List {
+                Section("Device Selection") {
+                    Toggle(isOn: $switchToAccessPoint) {
+                        Label("Connect Automatically", systemImage: "cpu")
+                    }
+                    .tint(.accentColor)
+                    
+                    if switchToAccessPoint {
+                        HStack {
+                            Label("Device SSID", systemImage: "wifi.circle")
+                            
+                            TextField("Access Point Name", text: $ssid)
+                                .foregroundColor(Color.universalAccentColor)
+                                .textInputAutocapitalization(.never)
+                                .disableAllAutocorrections()
+                                .frame(maxWidth: .infinity)
+                                .multilineTextAlignment(.trailing)
+                                .submitLabel(.done)
+                        }
                         
-                        TextField("Access Point Name", text: $ssid)
-                            .foregroundColor(Color.universalAccentColor)
-                            .textInputAutocapitalization(.never)
-                            .disableAllAutocorrections()
-                            .frame(maxWidth: .infinity)
-                            .multilineTextAlignment(.trailing)
-                            .submitLabel(.done)
+                        Label("Disable if you're already connected to the Device you'd like to provision or want to connect manually in Settings.", systemImage: "exclamationmark.triangle.fill")
+                            .font(.caption)
+                    } else {
+                        Label("Enable to automatically connect to the Device.", systemImage: "info.square.fill")
+                        
+                        Button("Set up Wi-Fi Connection in Settings") {
+                            guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+                            UIApplication.shared.open(url)
+                        }
+                        .frame(maxWidth: .infinity)
                     }
-                    
-                    Label("Disable if you're already connected to the Device you'd like to provision or want to connect manually in Settings.", systemImage: "exclamationmark.triangle.fill")
-                        .font(.caption)
-                } else {
-                    Label("Enable to automatically connect to the Device.", systemImage: "info.square.fill")
-                    
-                    Button("Set up Wi-Fi Connection in Settings") {
-                        guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
-                        UIApplication.shared.open(url)
-                    }
-                    .frame(maxWidth: .infinity)
                 }
             }
+            
+            Spacer()
+            
+            NavigationLink {
+                ProvisionOverWiFiView(switchToAccessPoint: switchToAccessPoint, ssidName: ssid)
+            } label: {
+                Label("Start Provisioning", systemImage: "arrowshape.forward.fill")
+                    .frame(maxWidth: .infinity)
+            }
+            .disabled(switchToAccessPoint && ssid.isEmpty)
+            .buttonStyle(.borderedProminent)
+            .padding(.horizontal, 16)
+            .padding(.vertical)
         }
         .navigationTitle("Provision over Wi-Fi")
-        
-        Spacer()
-        
-        NavigationLink {
-            ProvisionOverWiFiView(switchToAccessPoint: switchToAccessPoint, ssidName: ssid)
-        } label: {
-            Label("Start Provisioning", systemImage: "arrowshape.forward.fill")
-                .frame(maxWidth: .infinity)
-        }
-        .disabled(switchToAccessPoint && ssid.isEmpty)
-        .buttonStyle(.borderedProminent)
-        .padding(.horizontal, 16)
-        .padding(.vertical)
+        .background(Color.formBackground)
     }
 }
